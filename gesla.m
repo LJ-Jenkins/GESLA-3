3f2cecdb9490566a8cab5b74abfb902da1c8e7f2
Luke Jenkins <L.Jenkins@soton.ac.uk>
2022-05-09 11:51:05 +0100

    Update gesla.m

--- gesla.m
+++ gesla.m
@@ -1,21 +1,23 @@
 classdef gesla
     methods(Static)
 
-        function data = load_site(file,path,flag_removal)
+        function data = load_file(file,path,flag_removal)
             % Function to load data in GESLA-3 files
             % INPUT: 
             %    file -> string scalar or array with name/s of the individual file/s in GESLA format
             %    path -> directory to where the GESLA data files are kept
-            %    flag_removal - > 1 for removing wrong values, 2 for removing NULL
-            %    values, [1, 2] for removing both
+            %    flag_removal - > 'y' for removing values flagged by GESLA
             %
             % OUTPUT, struct 'data' containing for each site:
             %    ts -> time in datetime 
             %    sl -> tide gauge sea level measurements (with or w/o flag corrections)
-            %    f1 -> flag - column 4
-            %    f2 -> flag - column 5
+            %    cont_fl -> flag - contributor flags
+            %    gesla_fl -> flag - gesla checks, 0 = flagged value, 1 = correct value
             %    lat -> latitude
             %    lon -> longitude
+            %    datum -> datum information
+            %    gauge -> gauge type and information (e.g., 'bubbler - coastal')
+            %    cont_fl_info -> flag information from the contributor to be used with cont_fl
             % 
             % Removes wrong values as defined in metadata
             % Removes NULL values as defined in metadata
@@ -52,20 +54,13 @@ classdef gesla
                 %...Read sea level observations 
                 sl=A.data(:,1);
         
-                % remove incorrect values (according to flag in column 5)
-                f1 = A.data(:,2);
-                f2 = A.data(:,3);
-                if length(flag_removal) == 1
-                    switch flag_removal
-                        case 1
-                            sl(f1 == 0) = NaN;
-                        case 2
-                            sl(f2 == 0) = NaN;
-                    end
-                elseif length(flag_removal) > 1
-                    sl(f1 == 0) = NaN;
-                    sl(f2 == 0) = NaN;   
-                end     
+                % remove incorrect values (according to gesla flag)
+                cont_fl = A.data(:,2);
+                gesla_fl = A.data(:,3);
+
+                if strcmp(flag_removal, 'y')
+                    sl(gesla_fl == 0) = NaN;
+                end 
         
                 %...Read lat & lon
                 lat = str2double(extractAfter(A.textdata(~cellfun(@isempty,cellfun(@(x)strfind(x,'LATITUDE'),...
@@ -74,18 +69,33 @@ classdef gesla
                 lon = str2double(extractAfter(A.textdata(~cellfun(@isempty,cellfun(@(x)strfind(x,'LONGITUDE'),...
                     A.textdata(1:headerlength,1),'UniformOutput',0)),1),'# LONGITUDE'));
                 
+                %...Read metadata info
+                datum = strtrim(string(extractAfter(A.textdata(~cellfun(@isempty,cellfun(@(x)strfind(x,'DATUM INFORMATION'),...
+                    A.textdata(1:headerlength,1),'UniformOutput',0)),1),'# DATUM INFORMATION')));
+
+                gauge = strcat(strtrim(string(extractAfter(A.textdata(~cellfun(@isempty,cellfun(@(x)strfind(x,'INSTRUMENT'),...
+                    A.textdata(1:headerlength,1),'UniformOutput',0)),1),'# INSTRUMENT'))), " - ", ...
+                    strtrim(string(extractAfter(A.textdata(~cellfun(@isempty,cellfun(@(x)strfind(x,'GAUGE TYPE'),...
+                    A.textdata(1:headerlength,1),'UniformOutput',0)),1),'# GAUGE TYPE'))));
+
+                cont_fl_info = string(A.textdata(find(string(A.textdata(1:headerlength,1)) ==...
+                    '# Quality-control (QC) flags for column 4'):headerlength,1));
+
                 data.(strrep(i,'-','_')).ts = dt; 
                 data.(strrep(i,'-','_')).sl = sl; 
-                data.(strrep(i,'-','_')).f1 = f1; 
-                data.(strrep(i,'-','_')).f2 = f2; 
+                data.(strrep(i,'-','_')).cont_fl = cont_fl; 
+                data.(strrep(i,'-','_')).gesla_fl = gesla_fl; 
                 data.(strrep(i,'-','_')).lat = lat; 
                 data.(strrep(i,'-','_')).lon = lon;
+                data.(strrep(i,'-','_')).datum = datum;
+                data.(strrep(i,'-','_')).gauge = gauge;
+                data.(strrep(i,'-','_')).cont_fl_info = cont_fl_info;
         
             end
             
         end
 
-        function filenames = site_to_file(metadata,site_names)
+        function filenames = site_to_file(site_names,metadata)
             % Function to get the name/s of GESLA files from the site name/s 
             % INPUT: 
             %    metadata -> path to the metadata 'GESLA3_ALL.csv'
@@ -119,16 +129,18 @@ classdef gesla
             %    bounding_box -> [northern extent, southern extent, western extent, eastern extent]
             %    path -> directory to where the GESLA data files are kept
             %    metadata -> directory and filename for the metadata
-            %    flag_removal - > 1 for removing wrong values, 2 for removing NULL
-            %    values, [1, 2] for removing both
+            %    flag_removal - > 'y' for removing values flagged by GESLA
             %
             % OUTPUT, struct 'data' containing for each site:
             %    ts -> time in datetime 
             %    sl -> tide gauge sea level measurements (with or w/o flag corrections)
-            %    f1 -> flag - column 4
-            %    f2 -> flag - column 5
+            %    cont_fl -> flag - contributor flags
+            %    gesla_fl -> flag - gesla checks, 0 = flagged value, 1 = correct value
             %    lat -> latitude
             %    lon -> longitude
+            %    datum -> datum information
+            %    gauge -> gauge type and information (e.g., 'bubbler - coastal')
+            %    cont_fl_info -> flag information from the contributor to be used with cont_fl
             % 
             % Removes wrong values as defined in metadata
             % Removes NULL values as defined in metadata
@@ -139,7 +151,7 @@ classdef gesla
             filenames = string(md.FILENAME((md.LATITUDE >= bounding_box(2)  &  md.LATITUDE <= bounding_box(1)) &...
                 (md.LONGITUDE >= bounding_box(3)  &  md.LONGITUDE <= bounding_box(4))));
         
-            data = gesla.load_site(filenames,path,flag_removal);
+            data = gesla.load_file(filenames,path,flag_removal);
         
         end
 
@@ -150,16 +162,18 @@ classdef gesla
             %    n -> number of nearest sites to select
             %    path -> directory to where the GESLA data files are kept
             %    metadata -> directory and filename for the metadata
-            %    flag_removal - > 1 for removing wrong values, 2 for removing NULL
-            %    values, [1, 2] for removing both
+            %    flag_removal - > 'y' for removing values flagged by GESLA
             %
             % OUTPUT, struct 'data' containing for each site:
             %    ts -> time in datetime 
             %    sl -> tide gauge sea level measurements (with or w/o flag corrections)
-            %    f1 -> flag - column 4
-            %    f2 -> flag - column 5
+            %    cont_fl -> flag - contributor flags
+            %    gesla_fl -> flag - gesla checks, 0 = flagged value, 1 = correct value
             %    lat -> latitude
             %    lon -> longitude
+            %    datum -> datum information
+            %    gauge -> gauge type and information (e.g., 'bubbler - coastal')
+            %    cont_fl_info -> flag information from the contributor to be used with cont_fl
             % 
             % Removes wrong values as defined in metadata
             % Removes NULL values as defined in metadata
@@ -168,18 +182,91 @@ classdef gesla
             md = readtable(metadata);
 
             scoords = [md.LONGITUDE md.LATITUDE];
-            i = knnsearch(scoords,[-60 10],'K',n,'IncludeTies',true);
+            i = cell2mat(knnsearch(scoords,coords,'K',n,'IncludeTies',true))';
 
             filenames = string(md.FILENAME(i));
         
-            data = gesla.load_site(filenames,path,flag_removal);
+            data = gesla.load_file(filenames,path,flag_removal);
         
         end
 
+        function data = load_country(country,path,metadata,flag_removal)
+            % Function to load data in GESLA-3 files from specific country
+            % INPUT: 
+            %    country -> 3 letter country code used in GESLA, e.g. 'GBR' or 'JPN'
+            %               takes as many country codes as you give as either string or char array
+            %    n -> number of nearest sites to select
+            %    path -> directory to where the GESLA data files are kept
+            %    metadata -> directory and filename for the metadata
+            %    flag_removal - > 'y' for removing values flagged by GESLA
+            %
+            % OUTPUT, struct 'data' containing for each site:
+            %    ts -> time in datetime 
+            %    sl -> tide gauge sea level measurements (with or w/o flag corrections)
+            %    cont_fl -> flag - contributor flags
+            %    gesla_fl -> flag - gesla checks, 0 = flagged value, 1 = correct value
+            %    lat -> latitude
+            %    lon -> longitude
+            %    datum -> datum information
+            %    gauge -> gauge type and information (e.g., 'bubbler - coastal')
+            %    cont_fl_info -> flag information from the contributor to be used with cont_fl
+            % 
+            % Removes wrong values as defined in metadata
+            % Removes NULL values as defined in metadata
+            %... Luke Jenkins, May 2022
+        
+            md = readtable(metadata);
+
+            filenames = string(md.FILENAME(contains(md.COUNTRY,string(country))));
+        
+            data = gesla.load_file(filenames,path,flag_removal);
+        
+        end
+
+        function data = change_field_names(data,metadata)
+            % Function change the field names in an output data struct to different GESLA format
+            % INPUT: 
+            %    data -> 3 data struct output from one of above functions
+            %    metadata -> directory and filename for the metadata
+            %    Pop up options:
+            %                 'site' for GESLA site names
+            %                 'country' for country (MUST BE USED IN CONJUCTION WITH SITE AND/OR CODE e.g., 'site country')
+            %                 'cont' for abbreviated contributor (MUST BE USED IN CONJUCTION WITH SITE AND/OR CODE e.g., 'site code country cont')
+            %                 'code' for GESLA site codes - some codes start with numbers so therefore have to be used in conjuction and at end
+            %
+            % OUTPUT, struct 'data' with new field names
+            %... Luke Jenkins, May 2022
+
+            md = readtable(metadata);
+
+            dfields = string(strrep(fieldnames(data),'_',''));
+            files = string(replace(md.FILENAME,{'_','-'},{''}));
+
+            options = {string(md.SITENAME(contains(files, dfields)));...
+                string(md.COUNTRY(contains(files, dfields)));...
+                string(md.CONTRIBUTOR_ABBREVIATED_(contains(files, dfields)));...
+                string(md.SITECODE(contains(files, dfields)))};
+
+            prompt = {"site name","coutry","contributor (abbreviated)","site code"};
+            dlgtitle = "Select new field names (Place '1' in dialogue box)";
+            answer = inputdlg(prompt,dlgtitle,[1 length(char(dlgtitle))+35]);
+
+            options = options(~cellfun(@isempty, answer));
+            new_names = strings();
+
+            for i = 1:length(options)
+                switch i
+                    case 1
+                        new_names = append(new_names,string(options{i}));
+                    otherwise
+                        new_names = append(new_names,"_",string(options{i}));
+                end
+            end
+            
+            data = cell2struct(struct2cell(data), new_names);
+            
+        end
+    
     end
 
 end
-
-
-
-
